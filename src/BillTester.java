db2a24eed8471909c263751ba3330509e12cfdd2
WENWENWLHS <113708457+WENWENWLHS@users.noreply.github.com>
2022-11-08 09:32:12 -0800

    else if statement

--- src/BillTester.java
+++ src/BillTester.java
@@ -4,41 +4,46 @@ public class BillTester
     {
         double cost = 125.45;
 
-        // Create a bill for a birthday dinner for 10 people
 
+        // Create a bill for a birthday dinner for 10 people
+        Bill birthdayDinner = new Bill(cost * 10);
         System.out.println("Birthday Dinner");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
         // Add the tip
-
+        birthdayDinner.addTip();
         System.out.println("\nBirthday Dinner after tip");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
 
 
         System.out.println("\n\n"); // print a few blank lines
         // Another dinner with 5 guests
+        Bill birthdayDinner = new Bill(cost * 5);
         System.out.println("Birthday Dinner");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
         // Add the tip
+        birthdayDinner.addTip();
         // Notice the difference in the final amount
 
         System.out.println("\nBirthday Dinner after tip");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
 
 
         System.out.println("\n\n"); // print a few blank lines
         // Another dinner with 2 guests
+        Bill birthdayDinner = new Bill(cost * 2);
         System.out.println("Birthday Dinner");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
         // Add the tip
+        birthdayDinner.addTop();
         // Notice the difference in the final amount
 
         System.out.println("\nBirthday Dinner after tip");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
 
     }
